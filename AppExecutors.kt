a25fc72bb9ed888478c5cb87e1573e6cf50075c6
pouya <52459976+pouyarezaei@users.noreply.github.com>
2020-04-03 17:40:43 +0430

    Add files via upload

---
+++ AppExecutors.kt
@@ -0,0 +1,67 @@
+package com.pouyarezaei.backgroundgallery
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+/**
+ * Global executor pools for the whole application.
+ *
+ *
+ * Grouping tasks like this avoids the effects of task starvation (e.g. disk reads don't wait behind
+ * webservice requests).
+ *
+ *
+ * Creating a class like this is a way for android apps to handle non-trivial threading.
+ *
+ *
+ * Made By Pouya Rezaei
+ * Github : pouyarezaei
+ * RecyclerView Template Adapter
+ */
+class AppExecutors private constructor(
+    private val diskIO: ExecutorService,
+    private val networkIO: ExecutorService,
+    private val mainThread: Executor
+) {
+    fun diskIO(body: () -> Unit) = diskIO.execute(body)
+
+    fun mainThread(body: () -> Unit) = mainThread.execute(body)
+
+
+    fun networkIO(body: () -> Unit) = networkIO.execute(body)
+
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler =
+            Handler(Looper.getMainLooper())
+
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+    }
+
+    companion object {
+        // For Singleton instantiation
+        private val LOCK = Any()
+        private var sInstance: AppExecutors? = null
+        val instance: AppExecutors?
+            get() {
+                if (sInstance == null) {
+                    synchronized(LOCK) {
+                        sInstance = AppExecutors(
+                            Executors.newSingleThreadExecutor(),
+                            Executors.newFixedThreadPool(
+                                Runtime.getRuntime().availableProcessors() / 2
+                            ),
+                            MainThreadExecutor()
+                        )
+                    }
+                }
+                return sInstance
+            }
+    }
+
+}
